2909320e3c4c8c9b3b550c982c2e48a5ff57e8b3
LENOVO <LENOVO@LAPTOP-7M7CPIOL>
2022-07-27 18:34:39 +0700

    first commit

---
+++ src/main/java/Circle.java
@@ -0,0 +1,32 @@
+public class Circle extends Shape {
+    protected double radius;
+    public Circle(){
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle (double radius, String color , boolean filled){
+       super(color, filled);
+        this.radius = radius;
+    }
+    public double getadius(){
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    @Override
+    public double getArea() {
+        return Math.PI*Math.pow(radius, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+    @Override
+    public String toString(){
+        
+    return "A Circle with radius= "+ radius +", which is a subclass of yyy"+super.toString();
+    }
+}
